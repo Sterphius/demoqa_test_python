55ee4aa48b14f3ebd1a3a18d9e8efa420c856055
Sterphius <malyshff@gmail.com>
2023-02-04 23:31:26 +0300

    Add browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
-import pytest
 import os
-from selene.support.shared import browser
+
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene.support.shared import browser
 from dotenv import load_dotenv
 
 from demoqa_tests.utils import attachments
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -13,18 +23,19 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope="function", autouse=True)
-def open_browser():
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
 
     login = os.getenv('LOGIN')
@@ -37,11 +48,7 @@ def open_browser():
 
     browser.config.driver = driver
 
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
-    yield
+    yield browser
 
     attachments.add_html(browser)
     attachments.add_screenshot(browser)
