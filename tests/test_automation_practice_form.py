80c364bf10893cdc0d3744960fcc44173d6fa4e7
Sterphius <malyshff@gmail.com>
2023-02-26 20:25:43 +0300

    add allure steps

--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -1,11 +1,17 @@
 from demoqa_tests.model.pages.automation_practice_form import practice_form
 from tests.tests_data.user import User
+import allure
 
 
+@allure.title("Successful fill automation practice form")
 def test_submit_from():
     test_user = User(first_name='first', last_name='last')
-    practice_form.given_opened_wo_ads()
 
-    practice_form.fill_data(test_user).submit()
+    with allure.step("Open registrations form"):
+        practice_form.given_opened_wo_ads()
 
-    practice_form.should_have_registered(test_user)
+    with allure.step("Fill form"):
+        practice_form.fill_data(test_user).submit()
+
+    with allure.step("Verify registration form"):
+        practice_form.should_have_registered(test_user)
